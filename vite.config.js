db54eaaee159d4c4fd197b41076b2fd23eb036e8
Daniel Nakitare <dnakitare@gmail.com>
2024-05-16 16:49:42 -0600

    chore: Update Vite configuration with Vue plugin and resolve alias

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
